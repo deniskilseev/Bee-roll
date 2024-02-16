f7c3c0dd75e6899fd2fe65224e6e5ef8d1a30d33
Artemii Akhunov <artemii13052003@gmail.com>
2024-02-16 12:07:34 -0500

    Determined the train/val spit time

---
+++ machine_learning/src/config.py
@@ -0,0 +1,10 @@
+# All Paths are from ./src folder
+
+MOVIES_CSV_PATH = "../data/movies.csv"
+REVIEWS_CSV_PATH = "../data/ratings.csv"
+LINKS_CSV_PATH = "../data/links.csv"
+
+# determined from manual data explaration
+
+TRAIN_SPLIT_POINT = 1421944985
+TEST_SPLIT_POINT = 1515033908
