d32e245e25c0c13eba985ad4b3843121e50e0e20
Artemii Akhunov <artemii13052003@gmail.com>
2024-02-18 13:18:19 -0500

    Fixed data processing typo

--- machine_learning/src/data_processing.py
+++ machine_learning/src/data_processing.py
@@ -51,22 +51,19 @@ class UserHistory():
 def process_input(path_to_films, path_to_reviews, output_dir):
     films = pd.read_csv(path_to_films)
     reviews = pd.read_csv(path_to_reviews)
-    movie_rewiews = reviews.merge(films, on="movieId").sort_values(by="timestamp")
-    movie_reviews = movie_rewiews["timestamp"].apply(datetime.fromtimestamp)
-    movie_reviews = movie_rewiews["genres"].apply(parse_genres)
+    movie_reviews = reviews.merge(films, on="movieId").sort_values(by="timestamp")
+    movie_reviews["timestamp"] = movie_reviews["timestamp"].apply(datetime.fromtimestamp)
+    movie_reviews["genres"] = movie_reviews["genres"].apply(parse_genres)
     users_history = []
     for user_id in tqdm(movie_reviews["userId"].unique()):
-        user_reviews = movie_rewiews[movie_rewiews["userId"] == user_id]
+        user_reviews = movie_reviews[movie_reviews["userId"] == user_id]
         history = UserHistory(user_id)
-        for uid, movieid, rating, time, title, genre_list in user_reviews.iterrows():
-            movie = Movie(movie_id, title, genre_list)
-            review = Review(movie, time, rating)
+        for rid, row in user_reviews.iterrows():
+            movie = Movie(row.movieId, row.title, row.genres)
+            review = Review(movie, row.timestamp, row.rating)
             history.add_review(review)
         users_history.append(history)
     pickle.dump(users_history, os.path.join(output_dir, "processed.data"))
-
-
-
 
 
 def parse_genres(genres_list):
