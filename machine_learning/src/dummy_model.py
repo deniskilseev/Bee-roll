cd9fb4fa82b315aaf1b89baad76ec9d7c77b0d3d
Artemii Akhunov <artemii13052003@gmail.com>
2024-02-27 09:03:59 -0500

    Added a basic server with fastAPI and uvicorn

---
+++ machine_learning/src/dummy_model.py
@@ -0,0 +1,10 @@
+class DummyModel():
+    def __init__(self, dataframe):
+        self.sortedMovies = dataframe
+
+    def predict(self, x, nPredictions=50):
+        # Predict a for ratings
+        movieIds = self.sortedMovies["movie_id"][:len(x) + nPredictions]
+        answer = [movieId for movieId in movieIds if movieId not in x]
+        answer = pd.Series(answer[:nPredictions])
+        return answer
