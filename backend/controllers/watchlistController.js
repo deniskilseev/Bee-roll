61bb418fa80463eebf4cb78b2dd6ac36019e7089
Artemii Akhunov <artemii13052003@gmail.com>
2024-02-28 14:58:08 -0500

    Added methods to change the entry for the watchlist

--- backend/controllers/watchlistController.js
+++ backend/controllers/watchlistController.js
@@ -42,7 +42,7 @@ const watchListController = {
 
             console.log("WatchList created successfully: ", watch_list_title);
 
-            return res.status(200).json( {message: "Created WatchList successfully"} );
+            return res.status(201).json( {message: "Created WatchList successfully"} );
         } catch (error) {
             console.error("Error in createWatchList:", error);
             return res.status(500).json( {error: "Internal server error"} );
@@ -56,7 +56,7 @@ const watchListController = {
             const data_by_id = await WatchList.findOne( {watchListId: watch_list_id} );
 
             if (!data_by_id) {
-                return res.status(400).json( {error: "watchListId does not exits"} );
+                return res.status(400).json( {error: "watchListId does not exist"} );
             }
 
             return res.status(200).json({data_by_id});
@@ -69,8 +69,53 @@ const watchListController = {
 
     async editWatchList(req, res) {
         try {
-            const {watch_list_id, add_movies, ratings, delete_movies} = req.body;
+            const {watch_list_id, add_movie_ids, ratings, delete_movie_ids} = req.body;
 
+            const data_by_id = WatchList.findOne( {watchListId: watch_list_id} );
+
+            if (!data_by_id) {
+                return res.status(400).json( {error: "watchListId does not exist"} );
+            }
+            
+            if (add_movie_ids.length != ratings.length) {
+                return res.status(400).json( {error: "Amount of ratings is not the same as the nubmer of movies"} );
+            }
+
+            // check whether movie ids are valid.
+            const movie_data = await Movie.find( {movieId: $in add_movie_ids} );
+
+            if (movie_data.length != add_movie_ids.length) {
+                return res.status(400).json( {error: "Some movieId do not exist"} );
+            }
+
+            // Delete all movies that overlap.
+            
+            const movie_ids = data_by_id.movieIds;
+            const current_movie_ids_length = movie_ids.length;
+            const existing_ratings = data_by_id.ratings;
+
+            for (let i = 0; i < delete_movie_ids.length; i ++) {
+                for (let j = 0; j < current_movie_ids_length;) {
+                    if (movie_ids[j] == delete_movie_ids[i]) {
+                        movie_ids.splice(j, 1);
+                        existing_ratings.splice(j, 1);
+                        current_movie_ids_length --;
+                    } else {
+                        j ++;
+                    }
+                }
+            }
+
+            // Adding all of the other movie ratings
+            
+            movie_ids.push(add_movie_ids);
+            existing_ratings(ratings);
+
+            // Modify the entry in the DataBase:
+            
+            await WatchList.findOneAndUpdate( {watchListId: watch_list_id}, {movieIds: movie_ids, ratings: existing_ratings} );
+
+            return req.status(200).json( {message: "Modified watchlist successfully"} );
 
         } catch (error) {
             console.error("Error in editWatchList:". error);
