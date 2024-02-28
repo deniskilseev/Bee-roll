ef76ac6d79ff463ace6d0fa6e8c6b1b942a45403
Artemii Akhunov <artemii13052003@gmail.com>
2024-02-28 15:03:27 -0500

    Added a router for the watchlist

---
+++ backend/routes/watchlist.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const watchListController = require('../controllers/watchListController');
+
+router.post("/createWatchlist", watchListController.createWatchlist);
+router.get("/getWatchlist", watchListController.getWatchlist);
+router.post("/editWatchlist", watchListController.editWatchlist);
