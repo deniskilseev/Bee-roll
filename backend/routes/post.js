12c9909e110652328bb205d48979288d5413ef3f
Artemii Akhunov <artemii13052003@gmail.com>
2024-02-29 17:28:11 -0500

    Added get post info and delete post functionality

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const postController = require('../controllers/postController');
 
 router.post('/createPost', postController.createPost);
+router.get('/getPost/:post_id', postController.getPostInfo);
+router.delete('/deletePost/:post_id', postController.deletePost);
 
 // Export the router
 module.exports = router;
