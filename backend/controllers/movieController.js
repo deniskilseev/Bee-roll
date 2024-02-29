16d3643d0ad081f16dee2727f364c1e2023887e8
Artemii Akhunov <artemii13052003@gmail.com>
2024-02-29 10:23:56 -0500

    Added search function for the movies based on rexexp

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose')
 const express = require('express')
 const Movie = require('../model/Movie.js')
 
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+}
+
+
 const movieController = {
 
     async getMovieInfo(req, res) {
@@ -17,6 +22,21 @@ const movieController = {
             return res.status(200).json( {movie_data} );
         } catch (error) {
             console.error("Error in getMovieInfo:", error);
+            return res.status(500).json( {error: "Internal server error"} );
+        }
+    },
+
+    async findMoviesWithPattern(req, res) {
+        try {
+            const {pattern} = req.body;
+
+            const regex = new RegExp(escapeRegExp(pattern), "gi");
+
+            const foundMovies = await Movie.find( {title: regex} );
+
+            return res.status(200).json( {foundMovies} );
+        } catch (error) {
+            console.error("Error in findMoviesWithPattern:", error);
             return res.status(500).json( {error: "Internal server error"} );
         }
     }
