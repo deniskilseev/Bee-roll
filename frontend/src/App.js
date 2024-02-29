f1c574eeb590fa6cb492dc64eb48b48b0d7980a8
stiruchunapally <stiruchunapally@gmail.com>
2024-02-29 14:45:52 -0500

    Update profile based on logged in user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,29 @@ import Taskbar from './components/Taskbar';
 import ProfilePage from './Profile';
 import WatchlistPage from './WatchlistPage';
 import User from './components/User';
-import { UserProvider } from './UserContext';
+import { UserProvider, useUser } from './UserContext';
 
 const App = () => {
-  const dummyUser = {
+  const { user } = useUser();
+
+  const dummyUser = user
+  ? {
+      id: user.data_by_username.uid,
+      username: user.data_by_username.login,
+      bio: 'Example bio',
+      email: user.data_by_username.email,
+      profilePicture: 'blank profile pic.jpg',
+      followers: [],
+      following: [],
+      posts: [
+        { id: 1, title: 'First Post', content: 'First post.', author: user.data_by_username },
+        { id: 2, title: 'Second Post', content: 'Second post.', author: user.data_by_username },
+      ],
+    }
+  : 
+  {
     id: 1,
-    username: 'johndoe',
+    username: 'john_doe',
     bio: 'Example bio',
     email: 'john.doe@example.com',
     profilePicture: 'blank profile pic.jpg',
@@ -26,16 +43,14 @@ const App = () => {
 
   return (
     <Router>
-      <UserProvider>
-        <div className="app">
-          <Taskbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/profile" element={<ProfilePage user={dummyUser} />} />
-            <Route path="/watchlists" element={<WatchlistPage />} />
-          </Routes>
-        </div>
-      </UserProvider>
+      <div className="app">
+        <Taskbar />
+        <Routes>
+          <Route path="/" element={<HomePage user={dummyUser} />} />
+          <Route path="/profile" element={<ProfilePage user={dummyUser} />} />
+          <Route path="/watchlists" element={<WatchlistPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
