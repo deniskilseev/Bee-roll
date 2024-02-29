96f8ff8074d07fc694e229f27ccac86567e76ef2
Denis Kilseev <deniskilseev1@gmail.com>
2024-02-29 16:22:55 -0500

    createPost API created

--- backend/model/Post.js
+++ backend/model/Post.js
@@ -14,6 +14,10 @@ const postSchema = new Schema({
     forumId: {
         type: Number,
         required: true
+    },
+    postText: {
+        type: String,
+        required: true
     }
 });
 
