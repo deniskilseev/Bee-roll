a57c67d9706bf7089d27188584dbdaaa2d6ed044
Artemii Akhunov <artemii13052003@gmail.com>
2024-02-29 16:58:37 -0500

    Fixed routes for get in movied

--- backend/routes/movie.js
+++ backend/routes/movie.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movieController');
 
-router.get('/getInfo', movieController.getMovieInfo);
-router.get('/find', movieController.findMoviesWithPattern)
+router.get('/getInfo/:movie_id', movieController.getMovieInfo);
+router.get('/find/:pattern', movieController.findMoviesWithPattern)
 
 // Export the router
 module.exports = router;
