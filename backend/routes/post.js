257eddc75d4ea5a4cb0969a262389709e36d207c
Denis Kilseev <deniskilseev1@gmail.com>
2024-03-28 20:36:59 -0400

    get recent posts api + test

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -6,6 +6,8 @@ router.post('/createPost', postController.createPost);
 router.post('/pinPost', postController.pinPost);
 router.get('/getPost/:post_id', postController.getPostInfo);
 router.delete('/deletePost/:post_id', postController.deletePost);
+router.post('/getRecentPosts', postController.getRecentPosts);
+
 
 // Export the router
 module.exports = router;
