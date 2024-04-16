b3da3fe64424789f0a9a103eaf787962fd667e95
stiruchunapally <stiruchunapally@gmail.com>
2024-04-16 17:44:21 -0400

    Align buttons to right of container

--- frontend/src/components/Forum.js
+++ frontend/src/components/Forum.js
@@ -4,12 +4,10 @@ import {Link} from 'react-router-dom';
 const Forum = ({ forumName }) => {
     return (
         <div className="card my-3">
-            <div className="card-body">
-                <div className="d-flex align-items-center">
-                    <h5 className="card-title">{forumName}</h5>
-                    <div className="mb-3">
-                        <Link to={`/forums/${forumName}`} className="btn btn-primary">Take a Peek</Link>
-                    </div>
+            <div className="card-body d-flex justify-content-between align-items-center">
+                <h5 className="card-title">{forumName}</h5>
+                <div className="mb-3">
+                    <Link to={`/forums/${forumName}`} className="btn btn-primary">Take a Peek</Link>
                 </div>
             </div>
         </div>
